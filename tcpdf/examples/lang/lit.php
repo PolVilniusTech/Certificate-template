1fd91764a4caf7ded46b20538faf68b4f9648d6f
PolVilniusTech <76515269+PolVilniusTech@users.noreply.github.com>
2024-09-01 17:40:32 +0300

    Create lit.php

---
+++ tcpdf/examples/lang/lit.php
@@ -0,0 +1,45 @@
+<?php
+//============================================================+
+// File name   : lit.php
+// Begin       : 
+// Last Update : 
+//
+// Description : Language module for TCPDF
+//               (contains translated texts)
+//               Lithuanian
+//
+// Author: 
+//
+// (c) Copyright:
+//               Nicola Asuni
+//               Tecnick.com LTD
+//               www.tecnick.com
+//               info@tecnick.com
+//============================================================+
+
+/**
+ * TCPDF language file (contains translated texts).
+ * @package com.tecnick.tcpdf
+ * @brief TCPDF language file: Lithuania
+ * @author
+ * @since
+ */
+
+// Lithuanian
+
+global $l;
+$l = Array();
+
+// PAGE META DESCRIPTORS --------------------------------------
+
+$l['a_meta_charset'] = 'UTF-8';
+$l['a_meta_dir'] = 'ltr';
+$l['a_meta_language'] = 'lt';
+
+// TRANSLATIONS --------------------------------------
+$l['w_page'] = 'puslapis';
+
+//============================================================+
+// END OF FILE
+//============================================================+
+
